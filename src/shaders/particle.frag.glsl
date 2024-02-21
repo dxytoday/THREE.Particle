cad669b7da32dbb81de216b50f75a286ef42d02c
董旭阳 <dxytoday@163.com>
2024-02-21 15:05:05 +0800

    添加颜色的伽马校正

--- src/shaders/particle.frag.glsl
+++ src/shaders/particle.frag.glsl
@@ -18,6 +18,8 @@ uniform vec3 endColor;
 varying float vRandom;
 varying float vRatio;
 
+uniform bool srgbColorSpace;
+
 void main() {
 
     if(vRatio <= 0.0) {
@@ -56,5 +58,20 @@ void main() {
     }
 
     gl_FragColor = vec4(finalColor, opacity * finalOpacity);
+
+    // 理论上 if 里的代码需要始终执行
+    if(srgbColorSpace) {
+
+        // Linear sRGB 转换到 sRGB
+        // THREE.Color 内部默认会将颜色值从 sRGB 转换为 Linear sRGB
+        // sRGB 更符合人眼视觉，Linear sRGB 更适合做数学计算
+
+        vec3 greater = pow(gl_FragColor.rgb, vec3(0.41666)) * 1.055 - vec3(0.055);
+        vec3 lessAndEqual = gl_FragColor.rgb * 12.92;
+        vec3 flag = vec3(lessThanEqual(gl_FragColor.rgb, vec3(0.0031308)));
+
+        gl_FragColor.rgb = mix(greater, lessAndEqual, flag);
+
+    }
 
 }
